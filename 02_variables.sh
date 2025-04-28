c0513ce5850b499db7baf4ec4c0c6990d7b2a3b8
tejaswi <tejaswi338@gmail.com>
2025-04-28 12:50:31 +0530

    pushing secong script

---
+++ 02_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Teja
+PERSON2=Ravi
+
+echo "$PERSON1:: Hello Suresh, How are you?"
+echo "$PERSON2:: Hi Ramesh, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
