1faa1e3ad5a85a9258ae0ce18e451a9310069c74
tejaswi <tejaswi338@gmail.com>
2025-05-02 15:15:25 +0530

    passing functions script changes

--- 10_functions.sh
+++ 10_functions.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F -%H -%M -%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/home/ec2-user/devops/shell-scripts/$SCRIPTNAME_$TIMESTAMP.log
+
+
 if [ $USERID -ne 0 ]
 then
 echo "user is not superuser "
 exit 1
 else
-echo"user is super user"
+echo "user is super user"
 fi
 yum install mysql -y &>>$LOGFILE
 if [ $? -ne 0 ]
