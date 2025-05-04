0da95288ffe78059a712a91650c2192840de6ab4
tejaswi <tejaswi338@gmail.com>
2025-05-04 16:51:53 +0530

    passing 2 script changes

--- 13_installation.sh
+++ 13_installation.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/home/ec2-user/devops/shell-scripts/$SCRIPT_NAME-$TIMESTAMP.log
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/home/ec2-user/devops/shell-scripts/logs-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-    echo -e " $2....$R FAILURE $N "
-    exit
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
     else
-    echo -e " $2....$G completed successfully $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
-
 }
 
 if [ $USERID -ne 0 ]
 then
-echo " $R Install with super user $N "
-exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-echo -e " $G You are super user $N"
+    echo "You are super user."
 fi
 
-for i in @
+for i in $@
 do
-echo " package to install: $i "
-yum list installed $i &>>$LOGFILE
-if [ $? -eq 0 ]
-then
-echo -e " The package is installed .....$Y skipping it $N "
-else
-yum install $i -y &>>$LOGFILE
-VALIDATE $? "! Installation"
-fi
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
 done
