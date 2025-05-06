e6756509aa14c8346d8956691e5af1feb1f6239a
tejaswi <tejaswi338@gmail.com>
2025-05-06 13:58:46 +0530

    14,15

--- 14_currentscript.sh
+++ 14_currentscript.sh
@@ -2,6 +2,7 @@
 COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process id of current script : $$"
-./15_otherscript.sh
+#./15_otherscript.sh
+source ./15_otherscript.sh
 echo "After calling other script , course: $COURSE"
 
